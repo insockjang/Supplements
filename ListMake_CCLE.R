ed25d040a4bf4b629b6515933b4204ab66bfa66d
insockjang <ijang@belltown.fhcr.org>
2014-08-26 09:56:07 -0700

    Beta Coefficients Captured

--- ListMake_CCLE.R
+++ ListMake_CCLE.R
@@ -113,3 +113,39 @@ ListMake2<-function(drug.Type,data.Type,model.Type,ID){
   
   return(KKK) 
 }
+
+ListMake3<-function(drug.Type,data.Type,model.Type,ID){
+  a<-switch(drug.Type,
+            "ActArea" = "syn1757078",
+            "IC50" = "syn1757074")
+  
+  (a.1 = grep("E",data.Type))
+  (a.2 = grep("C",data.Type))
+  (a.3 = grep("Mh",data.Type))
+  
+  if(length(a.1)==1){
+    KKK<-list(list(name="bsSGLR_prior_synapse_beta.R", url="https://github.com/insockjang/SGLR/blob/master/bsSGLR_prior_synapse_beta.R", wasExecuted=T),
+              list(name="myData_CCLE_new.R", url="https://github.com/Sage-Bionetworks/PredictiveModel_pipeline/blob/master/myData_CCLE_new.R", wasExecuted=T),
+              list(name = "Expression",entity="syn1757082", wasExecuted=F),              
+              list(name = "Response",entity=a, wasExecuted=F)        
+    )    
+  }
+  
+  if(length(a.2)==1){
+    KKK<-list(list(name="bsSGLR_prior_synapse_beta.R", url="https://github.com/insockjang/SGLR/blob/master/bsSGLR_prior_synapse_beta.R", wasExecuted=T),
+              list(name="myData_CCLE_new.R", url="https://github.com/Sage-Bionetworks/PredictiveModel_pipeline/blob/master/myData_CCLE_new.R", wasExecuted=T),
+              list(name = "Copy Number Alteration",entity="syn1757086", wasExecuted=F),              
+              list(name = "Response",entity=a, wasExecuted=F)        
+    )        
+  }
+  
+  if(length(a.3)==1){
+    KKK<-list(list(name="bsSGLR_prior_synapse_beta.R", url="https://github.com/insockjang/SGLR/blob/master/bsSGLR_prior_synapse_beta.R", wasExecuted=T),
+              list(name="myData_CCLE_new.R", url="https://github.com/Sage-Bionetworks/PredictiveModel_pipeline/blob/master/myData_CCLE_new.R", wasExecuted=T),
+              list(name = "Mutation hybrid capture sequenceing",entity="syn1757084", wasExecuted=F),              
+              list(name = "Response",entity=a, wasExecuted=F)        
+    )    
+  }
+  
+  return(KKK) 
+}
--- ListMake_Sanger.R
+++ ListMake_Sanger.R
@@ -115,3 +115,40 @@ ListMake2<-function(drug.Type,data.Type,model.Type){
   
   return(KKK) 
 }
+
+
+ListMake3<-function(drug.Type,data.Type,model.Type){
+  a<-switch(drug.Type,
+            "ActArea" = "syn1807986",
+            "IC50" = "syn1742876") 
+  
+  (a.1 = grep("E",data.Type))
+  (a.2 = grep("C",data.Type))
+  (a.3 = grep("Mh",data.Type))
+  
+  if(length(a.1)==1){    
+    KKK<-list(list(name="bsSGLR_prior_synapse_beta.R", url="https://github.com/insockjang/SGLR/blob/master/bsSGLR_prior_synapse_beta.R", wasExecuted=T),
+              list(name="myData_Sanger.R", url="https://github.com/Sage-Bionetworks/PredictiveModel_pipeline/blob/master/myData_Sanger.R", wasExecuted=T),
+              list(name = "Expression",entity="syn1742878", wasExecuted=F),              
+              list(name = "Response",entity=a, wasExecuted=F)  
+    )    
+  }
+  
+  if(length(a.2)==1){
+    KKK<-list(list(name="bsSGLR_prior_synapse_beta.R", url="https://github.com/insockjang/SGLR/blob/master/bsSGLR_prior_synapse_beta.R", wasExecuted=T),
+              list(name="myData_Sanger.R", url="https://github.com/Sage-Bionetworks/PredictiveModel_pipeline/blob/master/myData_Sanger.R", wasExecuted=T),
+              list(name = "Copy Number Alteration",entity="syn1742880", wasExecuted=F),              
+              list(name = "Response",entity=a, wasExecuted=F)  
+    )       
+  }
+  
+  if(length(a.3)==1){
+    KKK<-list(list(name="bsSGLR_prior_synapse_beta.R", url="https://github.com/insockjang/SGLR/blob/master/bsSGLR_prior_synapse_beta.R", wasExecuted=T),
+              list(name="myData_Sanger.R", url="https://github.com/Sage-Bionetworks/PredictiveModel_pipeline/blob/master/myData_Sanger.R", wasExecuted=T),
+              list(name = "Mutation hybrid capture sequenceing",entity="syn2368559", wasExecuted=F),              
+              list(name = "Response",entity=a, wasExecuted=F)  
+    )    
+  }
+  
+  return(KKK) 
+}
